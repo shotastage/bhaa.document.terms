ba1498ad10bdd88297cf08fbb993e84bd03ec4a2
Shota Shimazu <shotastage@users.noreply.github.com>
2021-02-24 22:54:39 +0900

    Fix eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import { HomeDocument } from'./docs/Home';
 
-const App = (): JSX.Element => {
+const App: React.FC = (): JSX.Element => {
 
   return (
     <>
