970c0100786b71433af3d282d2c94e7a28433077
Shota Shimazu <shotastage@users.noreply.github.com>
2021-07-15 18:17:55 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ const App: React.FC = (): JSX.Element => {
   return (
     <>
       <h1>Bhaa Terms and Privacy Policy Page</h1>
-      <p>このページはBhaaの利用規約及びプライバシーポリシーを記述したページです。  </p>
-      <li>
-        <ul><a href="/terms">Terms of Use</a></ul>
-        <ul><a href="/priv">Privacy Policy</a></ul>
-      </li>
+      <p>このページはBhaaの利用規約及びプライバシーポリシーを記述したページです。</p>
+      <ul>
+        <li><a href="/terms">Terms of Use</a></li>
+        <li><a href="/priv">Privacy Policy</a></li>
+      </ul>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 import RouterComponent from "./Router";
 
