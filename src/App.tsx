38a405631219028a96dcc3ccbc2cb43d669bc3aa
Shota Shimazu <hornet.live.mf@gmail.com>
2022-05-05 00:19:17 +0900

    add link button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { List, Link } from './components/Link';
 
 const App: React.FC = (): JSX.Element => {
-
   return (
     <>
       <h1>Bhaa Terms and Privacy Policy Page</h1>
       <p>このページはBhaaの利用規約及びプライバシーポリシーを記述したページです。</p>
-      <ul>
-        <li><a href="/terms">Terms of Use</a></li>
-        <li><a href="/priv">Privacy Policy</a></li>
+      <ul style={{marginTop: "20px"}}>
+        <List><Link href="/terms">Terms of Use</Link></List>
+        <List><Link href="/priv">Privacy Policy</Link></List>
       </ul>
     </>
   );
---
+++ src/components/Link.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const List = styled.li`
+  list-style-type: none;
+  height: 4rem;
+`;
+
+export const Link = styled.a`
+  color: white;
+  background: #4444d8;
+  padding: 0.9rem 1rem;
+  border-radius: 0.5rem;
+  margin: 10px;
+
+  &:hover {
+    background: #454589;
+  }
+`;
